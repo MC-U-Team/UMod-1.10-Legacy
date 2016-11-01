1f20581eba28b2ddc87c81f9ad1c8923f7fcfba6
MrTroble <nifroh56@gmail.com>
2016-11-01 14:14:34 +0100

    Add IC2 Transformer and Exception catched

---
+++ src/main/java/net/hycrafthd/umod/ext/ic2/BlockIC2Transformer.java
@@ -0,0 +1,14 @@
+package net.hycrafthd.umod.ext.ic2;
+
+import net.hycrafthd.umod.block.BlockBaseMachine;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockIC2Transformer extends BlockBaseMachine{
+
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return null;
+	}
+	
+}
