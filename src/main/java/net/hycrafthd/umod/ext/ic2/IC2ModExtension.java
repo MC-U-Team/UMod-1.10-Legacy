f43b58caf37cce0452595106ac370bdbeca4d351
MrTroble <nifroh56@gmail.com>
2016-11-07 20:02:40 +0100

    IC2ext Update

--- src/main/java/net/hycrafthd/umod/ext/ic2/IC2ModExtension.java
+++ src/main/java/net/hycrafthd/umod/ext/ic2/IC2ModExtension.java
@@ -1,11 +1,16 @@
 package net.hycrafthd.umod.ext.ic2;
 
 import ic2.api.recipe.*;
+import ic2.core.block.ITeBlock;
+import ic2.core.block.TeBlockRegistry;
+import net.hycrafthd.corelib.registry.BlockRegistry;
+import net.hycrafthd.corelib.registry.TileEntityRegistry;
 import net.hycrafthd.corelib.util.ItemStackUtil;
 import net.hycrafthd.umod.*;
 import net.hycrafthd.umod.enumtype.EnumTypeBaseStuff;
 import net.hycrafthd.umod.ext.*;
 import net.hycrafthd.umod.ext.abs.*;
+import net.hycrafthd.umod.utils.ModRegistryUtils;
 import net.minecraft.block.Block;
 import net.minecraft.item.*;
 import net.minecraftforge.fml.common.event.*;
@@ -16,6 +21,19 @@ public class IC2ModExtension extends UmodExtension {
 	@Override
 	public AbstractOreDictionaryRegistry oredirctionary() {
 		return new IC2OreDictionary();
+	}
+	
+	public static Block transformer;
+	
+	@Override
+	public void init(FMLInitializationEvent evt) {
+		transformer = new BlockIC2Transformer().setUnlocalizedName("ic22uetransformer");
+		BlockRegistry.register(transformer, "ic22uetransformer");
+	}
+	
+	@Override
+	public void postinit(FMLPostInitializationEvent evt) {
+		TileEntityRegistry.register(TileEntityIC2Transformer.class);
 	}
 	
 	@Override
--- src/main/java/net/hycrafthd/umod/ext/ic2/TileEntityIC2Transformer.java
+++ src/main/java/net/hycrafthd/umod/ext/ic2/TileEntityIC2Transformer.java
@@ -1,10 +1,39 @@
 package net.hycrafthd.umod.ext.ic2;
 
+import ic2.api.energy.event.EnergyTileLoadEvent;
+import ic2.api.energy.event.EnergyTileUnloadEvent;
+import ic2.api.energy.tile.IEnergyAcceptor;
+import ic2.api.energy.tile.IEnergyConductor;
+import ic2.api.energy.tile.IEnergyEmitter;
+import ic2.api.energy.tile.IEnergySink;
+import ic2.api.energy.tile.IEnergySource;
+import ic2.core.block.ITeBlock;
+import ic2.core.block.TileEntityBlock;
+import ic2.core.block.comp.Energy;
 import net.hycrafthd.umod.api.energy.IPowerProvieder;
+import net.hycrafthd.umod.tileentity.TileEntityBase;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.MinecraftForge;
 
-public class TileEntityIC2Transformer extends TileEntity implements IPowerProvieder{
-
+public class TileEntityIC2Transformer extends TileEntity implements IPowerProvieder,IEnergyConductor,IEnergySource,IEnergySink{
+		
+	public TileEntityIC2Transformer() {
+	}
+	
+	@Override
+	public void onLoad() {
+		if(!worldObj.isRemote)
+		MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+	}
+	
+	@Override
+	public void onChunkUnload() {
+		super.onChunkUnload();
+		if(!worldObj.isRemote)
+		MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+	}
+	
 	@Override
 	public double getStoredPower() {
 		// TODO Auto-generated method stub
@@ -81,6 +110,82 @@ public class TileEntityIC2Transformer extends TileEntity implements IPowerProvie
 	public boolean isOutput() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public boolean emitsEnergyTo(IEnergyAcceptor receiver, EnumFacing side) {
+		return false;
+	}
+
+	@Override
+	public boolean acceptsEnergyFrom(IEnergyEmitter emitter, EnumFacing side) {
+		return false;
+	}
+
+	@Override
+	public double getConductionLoss() {
+		return 0;
+	}
+
+	@Override
+	public double getInsulationEnergyAbsorption() {
+		return 0;
+	}
+
+	@Override
+	public double getInsulationBreakdownEnergy() {
+		return 0;
+	}
+
+	@Override
+	public double getConductorBreakdownEnergy() {
+		return 0;
+	}
+
+	@Override
+	public void removeInsulation() {
+		
+	}
+
+	@Override
+	public void removeConductor() {
+		
+	}
+
+	@Override
+	public double getDemandedEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSinkTier() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double injectEnergy(EnumFacing directionFrom, double amount, double voltage) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getOfferedEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void drawEnergy(double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSourceTier() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
